26d084ee06f5f5555a130fa88b56aa08dda15f33
Laura Royle <lauraamie.royle@gmail.com>
2019-04-04 09:56:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array.pop
+  return array
+}

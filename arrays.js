8d48783bb376ffa40b25e88cccf360e0d89ff49d
Laura Royle <lauraamie.royle@gmail.com>
2019-04-04 09:51:23 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+    return array = [element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return array = [array,...element]
+}
